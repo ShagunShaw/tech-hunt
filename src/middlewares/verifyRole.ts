a31a5391708c3a0fc8638c92ed94fd4021ad1530
Shagun Shaw <shagunshaw2006@gmail.com>
2026-07-29 22:29:28 +0530

    feat: enhance role verification and game status checks; add detailed comments for clarity

--- src/middlewares/verifyRole.ts
+++ src/middlewares/verifyRole.ts
@@ -18,3 +18,5 @@ export const authorize = (...roles: string[]) => {
         }
     }
 }
+
+// We needed higher order function in case of this middleware only and not other middlewares because 'authorize' middleware takes some parameters like 'partcipant', 'admin', 'super-admin' whereas other middlewares do not. So, rule of thumb, always include a higher order function for the middlwares that takes some params in its function, else keep it like a normal function and no need to over-engineer it by making it a higher-order function
--- src/middlewares/verifyStartStatus.ts
+++ src/middlewares/verifyStartStatus.ts
@@ -1,8 +1,27 @@
-iss file mei we will check whether 'isStarted= true' h ya ni
+import type { NextFunction, Request, Response } from "express"
+import client from "../redis.config"
+import { apiError } from "../utils/ApiError"
 
+export const isRunning = async (req: any, res: Response, next: NextFunction) => {
+    try {
+        const startTime = Number(await client.get('game:startTime'))
+        // const startTime = time ? new Date(time) : null      // as redis always returns a string, so need to explicitly convert it to 'timestamp'
+        const running = await client.get('game:isRunning')
+        const duration = Number(await client.get('game:duration'))
 
-isRunning = false → block(handles game not started and super- admin ended it, both)
-isRunning = true AND currentTime - startTime < duration → allow
-isRunning = true AND currentTime - startTime >= duration → block(auto end)
+        // if(!startTime)      return res.status(500).json(new apiError(500, "Redis Error", "Start Time not found in Redis"))
+        if (!running) return res.status(400).json(new apiError(400, "Game not Started", "You cannot access this route as the game has not started now"))
 
-Additionally, also update the duartion value from 3 hr to(currentTime - StartTime) if super- admin ends the game, in the db, for any future reference, that exactly how much duration had been given to the game
+        if (running === 'true') {
+            const currentTime = new Date().getTime();
+
+            if ((currentTime - startTime) / 1000 >= duration) return res.status(403).json(new apiError(403, "Game Ended", "You now cannot access any game route as the game has been ended now"))
+        } else {
+            return res.status(403).json(new apiError(403, "Game Ended", "You cannot access this game route now as the game has been ended"))
+        }
+
+        return next();
+    } catch (error) {
+        return res.status(500).json(new apiError(500, "Unknown Error", "Something went wrong"))
+    }
+}
